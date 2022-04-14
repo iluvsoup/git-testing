9eebd842794edd51b59369a3fafbae8e902a270b
Edvin Loberg <edvin.loberg@gmail.com>
2022-04-14 20:44:06 +0200

    added epiclua index.lua! binary tree

---
+++ binarytree.lua
@@ -0,0 +1,47 @@
+local binarytree = {}
+binarytree.__index = binarytree
+
+function binarytree.new(number)
+    local tree = {
+        value = number,
+        left = nil,
+        right = nil
+    }
+
+    setmetatable(tree, binarytree)
+
+    return tree
+end
+
+function binarytree.from_list(list)
+    local tree = {binarytree.new(list[1])}
+
+    for index = 2, #list do
+        tree:insert(list[index])
+    end
+
+    return tree
+end
+
+function binarytree:insert(number)
+    local direction
+
+    direction = number <= self.value and "left" or "right"
+
+    if self[direction] then -- does something already exist there?
+        self[direction]:insert(number)
+    else
+        self[direction] = binarytree.new(number)
+    end
+end
+
+function binarytree:invert()
+    if not self.left and not self.right then return end
+
+    self.left, self.right = self.right, self.left
+
+    self.left:invert()
+    self.right:invert()
+end
+
+return binarytree
--- index.lua
+++ index.lua
@@ -1,1 +1,5 @@
-print("hello world")
+local binarytree = require("binarytree")
+local numbers = {5,12,7,3,21,2,8}
+
+local tree = binarytree.from_list(numbers)
+print(tree)
